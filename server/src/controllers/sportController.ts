ecddc2f5dc4a0210f230718fb67f73d8717edd05
Andres <arias.andres2321@gmail.com>
2024-10-05 23:22:04 -0400

    CRUD functionality for sports table

--- server/src/controllers/sportController.ts
+++ server/src/controllers/sportController.ts
@@ -4,7 +4,16 @@ import { FastifyReply } from "fastify/types/reply";
 
 const prisma = new PrismaClient();
 
-export async function getSports(request: FastifyRequest, reply: FastifyReply) {}
+export async function getSports(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const sports = await prisma.sport.findMany();
+    reply.send(sports);
+  } catch (error) {
+    return reply.status(500).send({
+      error: "An error occurred while fetching sports.",
+    });
+  }
+}
 
 export async function createSport(
   request: FastifyRequest,
@@ -26,4 +35,117 @@ export async function createSport(
   }
 }
 
-export async function findSport(request: FastifyRequest, reply: FastifyReply) {}
+export async function findSport(request: FastifyRequest, reply: FastifyReply) {
+  const { id } = request.params as { id: string };
+  try {
+    const sport = await prisma.sport.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return reply.status(200).send(sport);
+  } catch (error) {
+    return reply.status(500).send({
+      error: "An error occurred while fetching a sport.",
+    });
+  }
+}
+
+export async function updateSport(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { id } = request.params as { id: string };
+
+  // TODO: Create types for request body
+  const { positions, statTypes, leagues } = request.body as {
+    positions?: { name: string }[];
+    statTypes?: { name: string }[];
+    leagues?: {
+      name: string;
+      teams?: { name: string; players?: { name: string }[] }[];
+    }[];
+  };
+
+  try {
+    // Fetch the existing sport
+    const existingSport = await prisma.sport.findUnique({
+      where: { id: parseInt(id) },
+      include: {
+        positions: true,
+        statTypes: true,
+        leagues: {
+          include: {
+            teams: {
+              include: {
+                players: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!existingSport) {
+      return reply.status(404).send({ error: "Sport not found." });
+    }
+
+    const updatedSport = await prisma.sport.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        positions: {
+          create: positions?.map((position) => ({ name: position.name })),
+        },
+        statTypes: {
+          create: statTypes?.map((statType) => ({ name: statType.name })),
+        },
+        leagues: {
+          create: leagues?.map((league) => ({
+            name: league.name,
+            teams: league.teams
+              ? {
+                  create: league.teams.map((team) => ({
+                    name: team.name,
+                    players: team.players
+                      ? {
+                          create: team.players.map((player) => ({
+                            name: player.name,
+                          })),
+                        }
+                      : undefined,
+                  })),
+                }
+              : undefined,
+          })),
+        },
+      },
+    });
+
+    return reply.status(200).send(updatedSport);
+  } catch (error) {
+    return reply.status(500).send({
+      error: "An error occurred while updating a sport.",
+    });
+  }
+}
+
+export async function deleteSport(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { id } = request.params as { id: string };
+  try {
+    const sport = prisma.sport.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return reply.status(200).send(sport);
+  } catch (error) {
+    return reply.status(500).send({
+      error: "An error occurred while deleting a sport.",
+    });
+  }
+}
--- server/src/routes/sportRoutes.ts
+++ server/src/routes/sportRoutes.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from "fastify";
 import {
   createSport,
+  deleteSport,
   findSport,
   getSports,
+  updateSport,
 } from "../controllers/sportController";
 
-async function playerRoutes(server: FastifyInstance) {
+async function sportRoutes(server: FastifyInstance) {
   server.get("/", getSports);
   server.post("/create", createSport);
   server.get("/:id", findSport);
+  server.put("/:id", updateSport);
+  server.delete("/:id", deleteSport);
 }
 
-export default playerRoutes;
+export default sportRoutes;
