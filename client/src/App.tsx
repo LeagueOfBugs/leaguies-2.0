18b0753ae24fa72a7c4e046a6f6dfbc8d2a168de
Andres <arias.andres2321@gmail.com>
2024-10-09 01:00:35 -0400

    set up sub routes for player screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MobileNav from "./components/ui/navigation/mobile/MobileNav";
 import Header from "./components/ui/header/header";
 import SplashScreen from "./screens/SplashScreen";
-import PlayerScreen from "./screens/playerScreen/PlayerScreen";
+import PlayerScreen, {
+  Activity,
+  Record,
+  Roster,
+} from "./screens/playerScreen/PlayerScreen";
 import LeagueScreen from "./screens/LeagueScreen";
 import TeamScreen from "./screens/TeamScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -14,9 +18,14 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/home" element={<HomeScreen />} />
-        <Route path="/player" element={<PlayerScreen />} />
         <Route path="/league" element={<LeagueScreen />} />
         <Route path="/team" element={<TeamScreen />} />
+
+        <Route path="/player" element={<PlayerScreen />}>
+          <Route path="record" element={<Record />} />
+          <Route path="roster" element={<Roster />} />
+          <Route path="activity" element={<Activity />} />
+        </Route>
       </Routes>
       <MobileNav />
     </Router>
